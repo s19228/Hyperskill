b841cf77d1d6a33a5fd1d9c5034923fe7b70fd0c
Konrad Gryka <conradgry@gmail.com>
2021-01-06 16:18:20 +0100

    AreSiblings

---
+++ Battleship/Problems/Are siblings/src/Main.java
@@ -0,0 +1,12 @@
+import java.io.File;
+
+class Siblings {
+
+    public static boolean areSiblings(File f1, File f2) {
+        String parent1 = f1.getParent();
+        String parent2 = f2.getParent();
+
+        return parent1.equals(parent2);
+
+    }
+}
