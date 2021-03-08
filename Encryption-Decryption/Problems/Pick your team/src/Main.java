27f77917685006b0404675cff52be4504cbe0144
Konrad Gryka <conradgry@gmail.com>
2021-03-08 21:36:52 +0100

    Strategy Pick your team 20210308

---
+++ Encryption-Decryption/Problems/Pick your team/src/Main.java
@@ -0,0 +1,121 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+class SelectionContext {
+
+    private PersonSelectionAlgorithm algorithm;
+
+    public void setAlgorithm(PersonSelectionAlgorithm algorithm) {
+        this.algorithm = algorithm;
+    }
+
+    public Person[] selectPersons(Person[] persons) {
+        // write your code here
+        return algorithm.select(persons);
+    }
+}
+
+interface PersonSelectionAlgorithm {
+
+    Person[] select(Person[] persons);
+}
+
+class TakePersonsWithStepAlgorithm implements PersonSelectionAlgorithm {
+
+    int step;
+
+    public TakePersonsWithStepAlgorithm(int step) {
+        this.step = step;
+    }
+
+    @Override
+    public Person[] select(Person[] persons) {
+        if (persons.length == 1) {
+            return new Person[]{persons[0]};
+        } else {
+            ArrayList<Person> personArrayList = new ArrayList<>();
+
+            for (int i = 0; i < persons.length; i += step) {
+                personArrayList.add(persons[i]);
+            }
+            return (personArrayList.toArray(Person[]::new));
+
+        }
+    }
+}
+
+
+class TakeLastPersonsAlgorithm implements PersonSelectionAlgorithm {
+
+    private int count;
+
+    public TakeLastPersonsAlgorithm(int count) {
+        this.count = count;
+    }
+
+    @Override
+    public Person[] select(Person[] persons) {
+
+        if (persons.length == 1) {
+            return new Person[]{persons[0]};
+        } else {
+            ArrayList<Person> personArrayList = new ArrayList<>();
+
+            for (int i = persons.length-count; i < persons.length; i++) {
+                personArrayList.add(persons[i]);
+            }
+            return (personArrayList.toArray(Person[]::new));
+
+        }
+
+    }
+}
+
+class Person {
+
+    String name;
+
+    public Person(String name) {
+        this.name = name;
+    }
+}
+
+/* Do not change code below */
+public class Main {
+
+    public static void main(String[] args) {
+        final Scanner scanner = new Scanner(System.in);
+
+        final int count = Integer.parseInt(scanner.nextLine());
+        final Person[] persons = new Person[count];
+
+        for (int i = 0; i < count; i++) {
+            persons[i] = new Person(scanner.nextLine());
+        }
+
+        final String[] configs = scanner.nextLine().split("\\s+");
+
+        final PersonSelectionAlgorithm alg = create(configs[0], Integer.parseInt(configs[1]));
+        SelectionContext ctx = new SelectionContext();
+        ctx.setAlgorithm(alg);
+
+        final Person[] selected = ctx.selectPersons(persons);
+        for (Person p : selected) {
+            System.out.println(p.name);
+        }
+    }
+
+    public static PersonSelectionAlgorithm create(String algType, int param) {
+        switch (algType) {
+            case "STEP": {
+                return new TakePersonsWithStepAlgorithm(param);
+            }
+            case "LAST": {
+                return new TakeLastPersonsAlgorithm(param);
+            }
+            default: {
+                throw new IllegalArgumentException("Unknown algorithm type " + algType);
+            }
+        }
+    }
+}
